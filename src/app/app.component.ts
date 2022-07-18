02e83a2cb3af47f4ccd22804a8260ef442b3ec82
PuXter <lukagat1@gmail.com>
2022-07-18 13:00:04 +0200

    Todo Add and Delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,40 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Todo List';
 
+  todos = [
+    {
+      label: 'Be present',
+      done: true,
+      priority: 5
+    },
+    {
+      label: 'Do Todo-list',
+      done: false,
+      priority: 3
+    },
+    {
+      label: 'Administrator',
+      done: false,
+      priority: 5
+    },
+    {
+      label: 'Pacjent',
+      done: false,
+      priority: 5
+    }
+  ];
+
+  addTodo(newTodoLabel: any) {
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo: any) {
+    this.todos = this.todos.filter( t => t.label !== todo.label);
+  }
+
 }
